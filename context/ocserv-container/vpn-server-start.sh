83b70730f188ceb23c0b141e23935f7f224366c5
Benjamin Tovar <btovar@nd.edu>
2021-02-09 12:01:08 -0500

    set up conditional slirp4netns

--- context/ocserv-container/vpn-server-start.sh
+++ context/ocserv-container/vpn-server-start.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
-set -e
+set -ex
 
 source /etc/cms-vpn/vpn-common.sh
-
 
 # default options for ocserv.conf
 ocserv_conf_template=/etc/ocserv/ocserv.conf.template
 ocserv_conf_masq_template=/etc/ocserv/vpn-iptables.template
 ocserv_conf_port=${VPN_PORT:-8443}
-ocserv_conf_gateway=10.0.2.0/24             # default value from slirp4netns for tap0
+ocserv_conf_gateway=default   # default for ocserv
 ocserv_conf_default_domain=ocserv-server.net
 ocserv_conf_rx_bytes_sec=${VPN_RX_LIMIT:-1250000000}
 ocserv_conf_tx_bytes_sec=${VPN_TX_LIMIT:-1250000000}
@@ -22,8 +21,17 @@ ocserv_conf_masq_file=${ocserv_etc}/vpn-iptables
 ocserv_conf_passwd=${ocserv_etc}/ocserv.passwd
 ocserv_conf_certs="${ocserv_etc}"/certs
 ocserv_conf_pool=${VPN_POOL}
-ocserv_conf_interface=${VPN_XFACE}
 ocserv_conf_dns="${VPN_DNS}"
+
+USE_SLIRP=no
+if [[ -z "${VPN_INTERFACE}" ]]
+then
+    USE_SLIRP=yes
+    VPN_INTERFACE=tap0
+    ocserv_conf_gateway=10.0.2.0/24             # default value from slirp4netns for tap0
+fi
+
+ocserv_conf_interface=${VPN_INTERFACE}
 
 
 trap cleanup EXIT
@@ -89,22 +97,32 @@ then
     fi
 fi
 
+# Whether to create a network namespace for the network interface
+net_option=""
+if [[ "${USE_SLIRP}" = yes ]]
+then
+    net_option="--net"
+fi
+
 # Create the namespace where ocserv will run
-unshare --user --map-root-user --net --mount /bin/sh <<EOF &
+unshare --user --map-root-user ${net_option} --mount /bin/sh <<EOF &
 source /etc/cms-vpn/vpn-common.sh
 
 mount --bind /etc/ocserv/resolv.conf /etc/resolv.conf
 mount --bind /etc/ocserv/hosts.allow /etc/hosts.allow
 mount --bind /etc/ocserv/hosts.deny  /etc/hosts.deny
 
-# wait for slirp4netns
-wait_for_event 5 ip addr show ${ocserv_conf_interface}
+
+if [[ "${USE_SLIRP}" = yes ]]
+then
+    # wait for slirp4netns
+    wait_for_event 5 ip addr show ${ocserv_conf_interface}
+fi
 
 # setup masquerading defautls
 iptables-restore < ${ocserv_conf_masq_file}
 iptables -A INPUT -p tcp --dport ${ocserv_conf_port} -j ACCEPT
 iptables -A INPUT -p udp --dport ${ocserv_conf_port} -j ACCEPT
-#iptables -A INPUT -j DROP
 
 #launch ocserv
 export LD_PRELOAD=/usr/lib/keep_privileges.so
@@ -117,15 +135,18 @@ ocserv -f -c ${ocserv_conf_file}
 EOF
 echo $! > ${ns_pid}
 
-# Create virtual network interface in the namespace (tap0)
-slirp4netns --configure --mtu=65520 --disable-host-loopback $(cat ${ns_pid}) -a ${slirp_socket} tap0 &
-echo $! > ${slirp_pid}
+# Create tap0 if necessary
+if [[ "${USE_SLIRP}" = yes ]]
+then
+    # Create virtual network interface in the namespace (tap0)
+    slirp4netns --configure --mtu=65520 --disable-host-loopback $(cat ${ns_pid}) -a ${slirp_socket} tap0 &
+    echo $! > ${slirp_pid}
+    wait_for_event 5 stat ${slirp_socket}
 
-wait_for_event 5 stat ${slirp_socket}
-
-# Expose port $ocserv_conf_port. $ocserv_conf_port is used for incoming vpn client connections
-json_api='{"execute": "add_hostfwd", "arguments": {"proto": "tcp", "host_addr": "0.0.0.0", "host_port": '"${ocserv_conf_port}"', "guest_addr": "10.0.2.100", "guest_port": '"${ocserv_conf_port}"'}}'
-echo "${json_api}" | nc -U ${slirp_socket}
+    # Expose port $ocserv_conf_port. $ocserv_conf_port is used for incoming vpn client connections
+    json_api='{"execute": "add_hostfwd", "arguments": {"proto": "tcp", "host_addr": "0.0.0.0", "host_port": '"${ocserv_conf_port}"', "guest_addr": "10.0.2.100", "guest_port": '"${ocserv_conf_port}"'}}'
+    echo "${json_api}" | nc -U ${slirp_socket}
+fi
 
 wait
 
